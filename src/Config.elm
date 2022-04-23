d885dff44ffc9785e28e4305b8cca2d531dea3b3
Erik Mattfolk <erik.mattfolk@gmail.com>
2022-04-23 20:33:20 +0200

    Add config that mimics env
    There are some magic going on with the Lamdera environment that causes
    the LSP to complain when there is nothing to complain about. This limits
    the complaints to only the Config.elm file.

---
+++ src/Config.elm
@@ -0,0 +1,21 @@
+module Config exposing (..)
+
+import Env as LamderaEnv
+
+
+type Env
+    = Development
+    | Production
+
+
+{-| The current execution environment.
+It might look like this code is broken, but it works using _magic_.
+-}
+mode : Env
+mode =
+    case LamderaEnv.mode of
+        LamderaEnv.Development ->
+            Development
+
+        LamderaEnv.Production ->
+            Production
